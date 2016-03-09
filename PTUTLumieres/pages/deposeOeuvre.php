df32f49dfb64f68f495664ab4df27baff8a6d912
pablogabolde <pablo69580@gmail.com>
2016-03-09 20:31:48 +0100

    Mise en place des nouveaux champs dans le formulaire
    Sans gestion de la base de donnée

--- PTUTLumieres/pages/deposeOeuvre.php
+++ PTUTLumieres/pages/deposeOeuvre.php
@@ -24,16 +24,36 @@
 						Votre oeuvre sera ou non ensuite validée par le jury. </p>
 
 					<form method="POST" action="../templates/validationDeposeOeuvre.php" enctype="multipart/form-data">	
-						<label for="nomOeuvre"> Nom de votre Oeuvre* : </label> <br/>
+						<label for="nomOeuvre"> Nom de votre Oeuvre : </label> <br/>
 						<input class="inputText" type="text" name="nomOeuvre" required put-placeholder="test"; /> <br/>
 
-						<label for="nomArtiste"> Votre nom d'artiste* : </label> <br/>
+						<label for="nomArtiste"> Votre nom d'artiste : </label> <br/>
 						<input class="inputText" type="text" name="nomArtiste" required/> <br/>
 						
                         <label for="mail"> Votre e-mail : </label> <br/>
                         <input class="inputText" type="email" name="mail"  required/> <br/>
                         
-						<label for="lieu"> Le lieu de votre Oeuvre* : </label> <br/>
+                        <label for="budget"> Le budget estimé : </label> <br/>
+                        <input class="inputText" min="0" type="number" name="budget" required/> <br/>
+                        
+                        <label for="societeChoix"> Appartenez-vous à une société ? </label> <br/>
+                        <input type="radio" name="societeChoix"> Oui </input>
+                        <input type="radio" name="societeChoix" checked> Non </input> <br/>
+                        
+                        <label for="societe"> Le nom de votre société : </label> <br/>
+                        <input class="inputText" type="text" name="societe" required disabled /> <br/>
+                        
+                        <label for="superficie"> La superficie estimée : </label> <br/> 
+                        <input class="inputText" min="0" type="number" name="superficie" required/> <br/> 
+                        
+                        <label for="poidsChoix"> Avez-vous le poids de votre oeuvre ? </label> <br/>
+                        <input type="radio" name="poidsChoix"> Oui </input>
+                        <input type="radio" name="poidsChoix" checked> Non </input> <br/>
+        
+                        <label for="poids"> Entrez le poids : </label> <br/>
+                        <input class="inputText" type="number" min="0"  name="poids" required disabled/> <br/>
+                    
+						<label for="lieu"> Le lieu de votre Oeuvre : </label> <br/>
 						<select class="liste" name="lieu" size="1" required>
 							<?php
 								//On récupère la liste de tous les lieux
@@ -50,13 +70,11 @@
 
 						</select> <br/>
 						
-						<label for="photoOeuvre"> Envoyez nous une photo de l'oeuvre* : </label><br/>
+						<label for="photoOeuvre"> Envoyez nous une photo de l'oeuvre : </label><br/>
 						<input class="parcourir" type="file" name="photoOeuvre" required/><br/>
 
 						<input class="valider" type="submit" value="VALIDER" /> <br/>
-					</form>
-
-					<p> (*) Ces champs sont obligatoires </p>
+                    </form>
 
 					<?php
 						//On affiche le message si c'est bon ou non
