0dd0f9e343d73fdc98ca665d04142df1092ef44a
pablogabolde <pablo69580@gmail.com>
2016-03-10 17:01:14 +0100

    Ajout du téléchargement du document lors du dépot d'une oeuvre

--- PTUTLumieres/pages/deposeOeuvre.php
+++ PTUTLumieres/pages/deposeOeuvre.php
@@ -19,71 +19,86 @@
 			<div class="general">
 				<?php include("../templates/nav.html"); ?>
 				<div class="bloc">
-					<h1> POSTEZ VOTRE OEUVRE </h1>
-					<p class="textIntro"> Vous voulez poster votre oeuvre? Vous n'avez qu'à remplir ce formulaire. 
-						Votre oeuvre sera ou non ensuite validée par le jury. </p>
-
-					<form method="POST" action="../templates/validationDeposeOeuvre.php" enctype="multipart/form-data">	
-						<label for="nomOeuvre"> Nom de votre Oeuvre : </label> <br/>
-						<input class="inputText" type="text" name="nomOeuvre" required put-placeholder="test"; /> <br/>
-
-						<label for="nomArtiste"> Votre nom d'artiste : </label> <br/>
-						<input class="inputText" type="text" name="nomArtiste" required/> <br/>
-						
-                        <label for="mail"> Votre e-mail : </label> <br/>
-                        <input class="inputText" type="email" name="mail"  required/> <br/>
-                        
-                        <label for="budget"> Le budget estimé (€) : </label> <br/>
-                        <input class="inputText" min="0" type="number" name="budget" required/> <br/>
-                        
-                        <label for="societeChoix"> Appartenez-vous à une société ? </label> <br/>
-                        <input type="radio" name="societeChoix"> Oui </input>
-                        <input type="radio" name="societeChoix" checked> Non </input> <br/>
-                        
-                        <label for="societe"> Le nom de votre société : </label> <br/>
-                        <input class="inputText" id="societe" type="text" name="societe" required disabled /> <br/>
-                        
-                        <label for="superficie"> La superficie estimée (m²) : </label> <br/> 
-                        <input class="inputText" min="0" type="number" name="superficie" required/> <br/> 
-                        
-                        <label for="poidsChoix"> Avez-vous le poids de votre oeuvre ? </label> <br/>
-                        <input type="radio" name="poidsChoix"> Oui </input>
-                        <input type="radio" name="poidsChoix" checked> Non </input> <br/>
-        
-                        <label for="poids"> Entrez le poids (Kg) : </label> <br/>
-                        <input class="inputText" id="poids" type="number" min="0"  name="poids" required disabled/> <br/>
+					
                     
-						<label for="lieu"> Le lieu de votre Oeuvre : </label> <br/>
-						<select class="liste" name="lieu" size="1" required>
-							<?php
-								//On récupère la liste de tous les lieux
-								$resultats=$connexion->query('SELECT * FROM `lieu` ORDER by idLieu');
-								$resultats->setFetchMode(PDO::FETCH_OBJ);
-								while($resultat = $resultats->fetch())
-								{
-																	
-									echo '<option value="'.$resultat->lieu.'"> '. $resultat->lieu. '</option>';	
-								}
-
-								$resultats->closeCursor();
-							?>
-
-						</select> <br/>
-						
-						<label for="photoOeuvre"> Envoyez nous une photo de l'oeuvre : </label><br/>
-						<input class="parcourir" type="file" name="photoOeuvre" required/><br/>
-
-						<input class="valider" type="submit" value="VALIDER" /> <br/>
-                    </form>
-
-					<?php
+                    <?php
 						//On affiche le message si c'est bon ou non
 						if(isset($_GET['msg'])){
 							if($_GET['msg'] == 1){
-								echo '<p class="messageReussite"> Votre oeuvre à bien été ajouté, elle sera soumise au jury ! </p>';
+				    ?> 
+                            <script>alert("!!! Attention !!!\nVeuillez remplir le document en cliquant sur le lien qui va apparaître pour télécharger le document.\nN'oubliez surtout pas de le renvoyer à l'adresse indiquée dans celui ci! \nSans ce document, votre candidature n'a plus aucun sens."); </script>
+                    
+                            <p class="messageReussite"> Votre oeuvre à bien été ajouté, elle sera soumise au jury ! Pensez 
+                                à <a class="lienDeposeOeuvre" href="../vue/download/document.docx" download="document.docx"> télécharger le document.</a> </p> 
+                    <?php
 							}
 						}
+                        else{
+                    ?>
+                        <h1> POSTEZ VOTRE OEUVRE </h1>
+                        <p class="textIntro"> Vous voulez poster votre oeuvre? Vous n'avez qu'à remplir ce formulaire. 
+						Votre oeuvre sera ou non ensuite validée par le jury. </p>
+                    
+                        <form method="POST" action="../templates/validationDeposeOeuvre.php" enctype="multipart/form-data">	
+                            <label for="nomOeuvre"> Nom de votre Oeuvre : </label> <br/>
+                            <input class="inputText" type="text" name="nomOeuvre" required put-placeholder="test"; /> <br/>
+
+                            <label for="nomArtiste"> Votre nom d'artiste : </label> <br/>
+                            <input class="inputText" type="text" name="nomArtiste" required/> <br/>
+
+                            <label for="mail"> Votre e-mail : </label> <br/>
+                            <input class="inputText" type="email" name="mail"  required/> <br/>
+
+                            <label for="budget"> Le budget estimé (€) : </label> <br/>
+                            <input class="inputText" min="0" type="number" name="budget" required/> <br/>
+
+                            <label for="societeChoix"> Appartenez-vous à une société ? </label> <br/>
+                            <input type="radio" name="societeChoix"> Oui </input>
+                            <input type="radio" name="societeChoix" checked> Non </input> <br/>
+
+                            <label for="societe"> Le nom de votre société : </label> <br/>
+                            <input class="inputText" id="societe" type="text" name="societe" required disabled /> <br/>
+
+                            <label for="superficie"> La superficie estimée (m²) : </label> <br/> 
+                            <input class="inputText" min="0" type="number" name="superficie" required/> <br/> 
+
+                            <label for="poidsChoix"> Avez-vous le poids de votre oeuvre ? </label> <br/>
+                            <input type="radio" name="poidsChoix"> Oui </input>
+                            <input type="radio" name="poidsChoix" checked> Non </input> <br/>
+
+                            <label for="poids"> Entrez le poids (Kg) : </label> <br/>
+                            <input class="inputText" id="poids" type="number" min="0"  name="poids" required disabled/> <br/>
+
+                            <label for="lieu"> Le lieu de votre Oeuvre : </label> <br/>
+                            <select class="liste" name="lieu" size="1" required>
+                                <?php
+                                    //On récupère la liste de tous les lieux
+                                    $resultats=$connexion->query('SELECT * FROM `lieu` ORDER by idLieu');
+                                    $resultats->setFetchMode(PDO::FETCH_OBJ);
+                                    while($resultat = $resultats->fetch())
+                                    {
+
+                                        echo '<option value="'.$resultat->lieu.'"> '. $resultat->lieu. '</option>';	
+                                    }
+
+                                    $resultats->closeCursor();
+                                ?>
+
+                            </select> <br/>
+
+                            <label for="photoOeuvre"> Envoyez nous une photo de l'oeuvre : </label><br/>
+                            <input class="parcourir" type="file" name="photoOeuvre" required/><br/>
+
+                            <input class="valider" type="submit" value="VALIDER" /> <br/>
+                        </form>
+                    <?php
+                        }
 					?>
+					
+
+					
+
+					
 				</div>
 			</div>
             
